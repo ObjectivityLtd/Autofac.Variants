d429819bda0986b8048ec622f308876a895700b6
jkaczmarek2 <jkaczmarek2@objectivity.co.uk>
2017-10-26 08:28:58 +0200

    EmbeddedResource class refactoring

--- Objectivity.Bot.Plugins/Resources/Models/EmbeddedResource.cs
+++ Objectivity.Bot.Plugins/Resources/Models/EmbeddedResource.cs
@@ -1,11 +1,8 @@
 ﻿namespace Objectivity.Bot.Plugins.Resources.Models
 {
     using System;
-    using System.Globalization;
-    using System.IO;
-    using System.Linq;
-    using System.Reflection;
     using System.Resources;
+    using Extensions;
 
     [Serializable]
     public class EmbeddedResource
@@ -32,44 +29,6 @@
             var value = this.GetString(key);
 
             return !string.IsNullOrEmpty(value);
-        }
-
-        private ResourceManager GetResourceManager()
-        {
-            if (string.IsNullOrEmpty(this.AssemblyName) || string.IsNullOrEmpty(this.ResourceAssemblyPath))
-            {
-                return null;
-            }
-
-            var baseName = Path.GetFileNameWithoutExtension(this.ResourceAssemblyPath);
-
-            if (string.IsNullOrEmpty(baseName))
-            {
-                return null;
-            }
-
-            var resourceAssembly = this.GetResourceAssembly();
-
-            return new ResourceManager(baseName, resourceAssembly);
-        }
-
-        private Assembly GetResourceAssembly()
-        {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            var resourceAssembly = assemblies
-                .FirstOrDefault(assembly => assembly.GetName().Name == this.AssemblyName);
-
-            if (resourceAssembly == null)
-            {
-                var exceptionMessage = string.Format(
-                    CultureInfo.CurrentCulture,
-                    "Couldn't find assembly named {0}.",
-                    this.AssemblyName);
-
-                throw new ArgumentException(exceptionMessage);
-            }
-
-            return resourceAssembly;
         }
     }
 }
